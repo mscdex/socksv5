7187d06c9d3945e9f17338dc5b54e43a2ffc220b
Brian White <mscdex@mscdex.net>
2014-07-13 16:57:09 -0400

    lib: add Client

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     path = require('path');
 
-['server'].forEach(function(f) {
+['server', 'client'].forEach(function(f) {
   var exp = require('./lib/' + f),
       keys = Object.keys(exp);
   for (var i = 0, len = keys.length; i < len; ++i)
